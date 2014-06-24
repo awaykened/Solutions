06cd250ba774a9cc757cae06d32798d6cdf5cb3c
WeiWei <Way@WeiWeis-MacBook-Air.local>
2014-06-23 20:20:10 -0400

    Initial Commit

---
+++ foxlings/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  foxlings
+//
+//  Created by WeiWei on 2014-06-23.
+//  Copyright (c) 2014 WeiWei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
